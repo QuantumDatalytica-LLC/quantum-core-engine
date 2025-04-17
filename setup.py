7c578c777db6ff6c3777169780e7f8beb8cfcae8
Bhanderi-Aksahy <akshay.bhanderi@softqubes.com>
2025-04-17 12:24:27 +0530

    Code added from mac - Version 1.0.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantum-core-engine",
-    version="1.0.6",
+    version="1.0.9",
     packages=find_packages(),
     include_package_data=True,  
     package_data={
